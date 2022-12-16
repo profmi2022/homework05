be9415e4893778a5b110c07c377e01712d9f9b7c
profmi2022 <profmi2022@gmail.com>
2022-12-17 00:40:52 +0600

    Сделана ДЗ 5 урока

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        task1();
+        task2();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+
+        System.out.println(fullName);
+
+        System.out.println("Задача 2");
+
+        System.out.println(fullName.toUpperCase());
+    }
+
+    public static void task2() {
+        System.out.println("Задача 3");
+
+        String fullName = "Иванов Семён Семёнович";
+
+        String newFullName = fullName.replaceAll ("ё","е");
+
+        System.out.println(newFullName);
     }
 }
